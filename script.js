4c652d7cb878d0d5b6b53cf899c7d8087ba57afe
Rupesh Sagaram <s.v.rupesh97@gmail.com>
2024-08-05 21:00:02 +0530

    added a div for the container and center aligned the container so that all the divs appear in the center of the page

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ for(let i=0; i<17; i++){
      divGrid.classList.add('divGrid');
      divGrid.style.backgroundColor= 'red';
      divGrid.style.display = 'flex';
-     divGrid.style.height = '50px';
-     divGrid.style.width = '50px';
+     divGrid.style.height = '100px';
+     divGrid.style.width = '100px';
     for(let j=0; j<17; j++){
     let divGrid = document.createElement('div');
      divGrid.classList.add('divGrid');
      divGrid.style.backgroundColor= 'red';
      divGrid.style.display = 'flex';
-     divGrid.style.height = '50px';
-     divGrid.style.width = '50px';
+     divGrid.style.height = '100px';
+     divGrid.style.width = '100px';
      container.appendChild(divGrid);
     }
 }

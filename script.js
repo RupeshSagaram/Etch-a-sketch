d66c539e48693776e82db7c52f8fd17b556520c5
Rupesh Sagaram <s.v.rupesh97@gmail.com>
2024-08-07 21:05:39 +0530

    added hover feature and it is functional

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 let container = document.querySelector('#container');
 
-for(let i=0; i<17; i++){
-    let divGrid = document.createElement('div');
+for(let i=0; i<=16; i++){
+    /*let divGrid = document.createElement('div');
      divGrid.classList.add('divGrid');
      divGrid.style.backgroundColor= 'red';
      divGrid.style.display = 'flex';
-     divGrid.style.height = '100px';
-     divGrid.style.width = '100px';
-    for(let j=0; j<17; j++){
+     divGrid.style.height ='auto';
+     divGrid.style.width = '60px';
+     */
+    for(let j=0; j<=16; j++){
     let divGrid = document.createElement('div');
      divGrid.classList.add('divGrid');
-     divGrid.style.backgroundColor= 'red';
+     divGrid.style.backgroundColor= 'cadetblue';
      divGrid.style.display = 'flex';
-     divGrid.style.height = '100px';
-     divGrid.style.width = '100px';
+     divGrid.style.height = '60px';
+     divGrid.style.width = '60px'
      container.appendChild(divGrid);
     }
 }
+ document.querySelectorAll('.divGrid').forEach(divGrid =>{
+    divGrid.addEventListener('mouseover',()=>{
+    divGrid.style.backgroundColor = 'white';
+     }
+    )}
+  );
+

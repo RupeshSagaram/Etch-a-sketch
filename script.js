1395ed3b9f5aba30f3896958ae4cfa43366ea221
Rupesh Sagaram <s.v.rupesh97@gmail.com>
2024-08-10 11:35:11 +0530

    created random color function and added it to the mouseover events

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 let container = document.querySelector('#container');
 let btn = document.querySelector('.btn');
 
+//randomizing the square colors.
 
+function randomColor(){
+
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    let randomRGB = "rgb(" + x + ','+ y + ',' + z +')';
+    return randomRGB;
+}
 
 for(let i =0; i<=16; i++){
     let divRow = document.createElement('div');
@@ -11,21 +20,20 @@ for(let i =0; i<=16; i++){
     for(let j=0; j<=16; j++){
         let divColumn = document.createElement('div');
         divColumn.classList.add('column');
-        divColumn.style.backgroundColor = 'red';
+        divColumn.style.backgroundColor = 'cadetblue';
         divColumn.style.padding = '1.5rem','0';
         divRow.appendChild(divColumn); 
-    }
-    
+    }  
      
 }
 document.querySelectorAll('.column').forEach(divColumn =>{
     divColumn.addEventListener('mouseover',()=>{
-        divColumn.style.backgroundColor = 'white';
+        divColumn.style.backgroundColor = randomColor();
      }
     )}
   );
 
-  //the following is the repeated code for the button event.
+  //the following is the repeated for condition for the button event.
   btn.addEventListener('click',()=>{
     
     squaresPerSide= +prompt(`how many squares per side for the new grid?`);
@@ -50,7 +58,7 @@ if(squaresPerSide<100){
             for(let j=0; j<squaresPerSide; j++){
                 let divColumn = document.createElement('div');
                 divColumn.classList.add('column');
-                divColumn.style.backgroundColor = 'red';
+                divColumn.style.backgroundColor = 'cadetblue';
                 divColumn.style.padding = '1rem','0';
                 divColumn.style.flex = '1';
                 divRow.appendChild(divColumn); 
@@ -58,7 +66,7 @@ if(squaresPerSide<100){
         }
         document.querySelectorAll('.column').forEach(divColumn =>{
             divColumn.addEventListener('mouseover',()=>{
-                divColumn.style.backgroundColor = 'white';
+                divColumn.style.backgroundColor = randomColor();
              }
             )}
           );
@@ -67,6 +75,9 @@ if(squaresPerSide<100){
     squaresPerSide= alert(`sorry! only values under 100 is acceptable`);
     }
         });
+
+
+
 
 
 

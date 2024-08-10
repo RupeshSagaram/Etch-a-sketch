a6627cf5ec441095a63819483389a963d8ce8797
Rupesh Sagaram <s.v.rupesh97@gmail.com>
2024-08-10 08:40:15 +0530

    all the events in button are working, existing elements are deleted and new ones are created however, there's an issue with small grid values, it's not aligned correctly. need to work on it.

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ let btn = document.querySelector('.btn');
 
 
 
-for(let i =0; i<16; i++){
+for(let i =0; i<=16; i++){
     let divRow = document.createElement('div');
     divRow.classList.add('row');
     divRow.style.display = 'flex';
     container.appendChild(divRow);
-    for(let j=0; j<16; j++){
+    for(let j=0; j<=16; j++){
         let divColumn = document.createElement('div');
         divColumn.classList.add('column');
         divColumn.style.backgroundColor = 'red';
@@ -25,10 +25,45 @@ document.querySelectorAll('.column').forEach(divColumn =>{
     )}
   );
 
+  //the following is the repeated code for the button event.
   btn.addEventListener('click',()=>{
-    squaresPerSide=prompt(`how many squares per side for the new grid?`);
-    //return squaresPerSide;
-  })
+    
+    squaresPerSide= +prompt(`how many squares per side for the new grid?`);
+
+    document.querySelectorAll('.row').forEach(divRow =>{
+        
+        container.removeChild(divRow)
+    }
+  );
+
+  document.querySelectorAll('.column').forEach(divColumn =>{
+        
+    container.removeChild(divColumn);
+} )
+
+    for(let i =0; i<squaresPerSide; i++){
+        let divRow = document.createElement('div');
+        divRow.classList.add('row');
+        divRow.style.display = 'flex';
+        container.appendChild(divRow);
+        for(let j=0; j<squaresPerSide; j++){
+            let divColumn = document.createElement('div');
+            divColumn.classList.add('column');
+            divColumn.style.backgroundColor = 'red';
+            divColumn.style.padding = '1.5rem','0';
+            divRow.appendChild(divColumn); 
+        }
+
+        document.querySelectorAll('.column').forEach(divColumn =>{
+            divColumn.addEventListener('mouseover',()=>{
+                divColumn.style.backgroundColor = 'white';
+             }
+            )}
+          );
+    
+  }});
+
+  
 
 
 
